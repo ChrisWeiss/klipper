f0f9de677b0bbaabf2ba236adf414e451d11c012
Kevin O'Connor <kevin@koconnor.net>
2026-07-27 21:39:23 -0400

    reactor: Add support for detecting high latency
    Add support to the reactor for reporting if a set of callbacks take a
    long time before returning to the main reactor loop.
    
    Signed-off-by: Kevin O'Connor <kevin@koconnor.net>

--- klippy/reactor.py
+++ klippy/reactor.py
@@ -129,6 +129,11 @@ class SelectReactor:
         self._cached_dispatch_greenlets = []
         self._all_greenlets = []
         self._prevent_pause_count = 0
+        # Tracking of high latency
+        self._latency_warning = self.NEVER
+        self._latency_callback = (lambda e, pe, rc: None)
+        self._recent_eventtime = 0.
+        self._recent_callbacks = []
     # Timers
     def update_timer(self, timer_handler, waketime):
         if timer_handler.timer_is_running:
@@ -155,6 +160,7 @@ class SelectReactor:
             if eventtime >= waketime:
                 t.waketime = self.NEVER
                 t.timer_is_running = True
+                self._recent_callbacks.append(t.callback)
                 t.waketime = waketime = t.callback(eventtime)
                 t.timer_is_running = False
                 if g_dispatch is not self._g_dispatch:
@@ -282,16 +288,34 @@ class SelectReactor:
         for fd, event in hdls:
             hdl = self._fds.get(fd, self._dummy_fd_hdl)
             if event & self._READ:
+                self._recent_callbacks.append(hdl.read_callback)
                 hdl.read_callback(eventtime)
                 if g_dispatch is not self._g_dispatch:
                     self._end_greenlet(g_dispatch)
                     return True
             if event & self._WRITE:
+                self._recent_callbacks.append(hdl.write_callback)
                 hdl.write_callback(eventtime)
                 if g_dispatch is not self._g_dispatch:
                     self._end_greenlet(g_dispatch)
                     return True
         return False
+    # High latency checking
+    def set_latency_notifier(self, latency, latency_callback):
+        self._latency_warning = latency
+        self._latency_callback = latency_callback
+    def _check_latency(self, eventtime, timeout):
+        prev_eventtime = self._recent_eventtime
+        self._recent_eventtime = eventtime
+        if timeout or eventtime - prev_eventtime < self._latency_warning:
+            # High latency not detected - prep for next pass
+            del self._recent_callbacks[:]
+            return False
+        # High latency detected - invoke callback
+        prev_cbs = self._recent_callbacks
+        self._recent_callbacks = [self._latency_callback]
+        self._latency_callback(eventtime, prev_eventtime, prev_cbs)
+        return True
     # Main loop
     def _dispatch_loop(self):
         eventtime = 0.
@@ -300,13 +324,15 @@ class SelectReactor:
             # Check if can sleep
             timeout = 0.
             if not busy:
+                self._recent_callbacks.append(self._idle_callback)
                 busy = self._idle_callback(eventtime)
                 if not busy:
                     timeout = min(1., max(.001, self._next_timer - eventtime))
-            busy = False
             # Check for file activity
             hdls = self._check_fd_activity(timeout)
             eventtime = self.monotonic()
+            # Check for high latency
+            busy = self._check_latency(eventtime, timeout)
             # Dispatch file events
             if hdls:
                 busy = True
@@ -322,6 +348,8 @@ class SelectReactor:
             self._setup_async_callbacks()
         self._process = True
         self._prevent_pause_count = 0
+        self._recent_eventtime = self.monotonic()
+        self._recent_callbacks = []
         try:
             while self._process:
                 # Create new greenlet to dispatch timers and events
@@ -332,6 +360,7 @@ class SelectReactor:
                 # Control returns here on end() request or switch from pause()
         finally:
             self._g_dispatch = None
+        self._recent_callbacks = []
     def end(self):
         self._process = False
     def finalize(self):
